6e55000758d6b5a400110098880c23d7cc836efb
NGOVANMANH <vanmanh0888@gmail.com>
2023-11-24 11:30:51 +0700

    delete comment

--- src/components/Slide/index.js
+++ src/components/Slide/index.js
@@ -16,22 +16,6 @@ const Slide = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
             ))}
-            {/* <Carousel.Item interval={500}>
-                <img src="" alt="slide 2" />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src="" alt="slide 3" />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item> */}
         </Carousel>
     );
 }
